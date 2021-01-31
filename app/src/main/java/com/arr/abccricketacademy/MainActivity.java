46fc32095599745f3e09fe10789c3a008e03b98e
somasekharanaparthi <shhekarchowdary@gmail.com>
2021-02-01 02:50:10 +0530

    2 Layouts Completed

--- app/src/main/java/com/arr/abccricketacademy/MainActivity.java
+++ app/src/main/java/com/arr/abccricketacademy/MainActivity.java
@@ -2,13 +2,32 @@ package com.arr.abccricketacademy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mAdd_Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAdd_Button = findViewById(R.id.add_button);
+
+        mAdd_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), add_player.class);
+                startActivity(intent);
+
+            }
+        });
+
+
     }
 }
