adab49d937f886d352528e9a9aa99625dd0e50f6
Manikantareddy1 <51731900+Manikantareddy1@users.noreply.github.com>
2021-02-02 02:35:47 +0530

    hello

--- app/src/main/java/com/arr/abccricketacademy/activity_view_player.java
+++ app/src/main/java/com/arr/abccricketacademy/activity_view_player.java
@@ -1,7 +1,9 @@
 package com.arr.abccricketacademy;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -44,6 +46,39 @@ public class activity_view_player extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+            }
+        });mGetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor res = DB.getdata();
+                if (res.getCount() == 0) {
+                    Toast.makeText(activity_view_player.this, "No Entry Exists", Toast.LENGTH_SHORT).show();
+                    return;
+
+                }
+                StringBuffer buffer = new StringBuffer();
+                while (res.moveToNext()) {
+                    buffer.append("Player Id :" + res.getString(0) + "/n");
+                    buffer.append("First Name :" + res.getString(1) + "/n");
+                    buffer.append("Last Name :" + res.getString(2) + "/n/n");
+                    buffer.append("Dob :" + res.getString(3) + "/n/n/n");
+                    buffer.append("Height :" + res.getString(4) + "/n/n/n/n");
+                    buffer.append("Weight :" + res.getString(5) + "/n/n/n/n/n");
+                    buffer.append("Skill :" + res.getString(6) + "/n/n/n/n/n/n");
+                    buffer.append("House No :" + res.getString(7) + "/n/n/n/n/n/n/n");
+                    buffer.append("Street :" + res.getString(8) + "/n/n/n/n/n/n/n/n");
+                    buffer.append("City :" + res.getString(9) + "/n/n/n/n/n/n/n/n/n");
+                    buffer.append("Zip Code :" + res.getString(10) + "/n/n/n/n/n/n/n/n/n/n");
+                    buffer.append("Academy Id :" + res.getString(11) + "/n/n/n/n/n/n/n/n/n/n/n/");
+                    buffer.append("Coach Id :" + res.getString(12) + "/n/n/n/n/n/n/n/n/n/n/n/n");
+                    buffer.append("Team Id :" + res.getString(13) + "/n/n/n/n/n/n/n/n/n/n/n/n/n");
+
+                }
+                AlertDialog.Builder builder = new AlertDialog.Builder(activity_view_player.this);
+                builder.setCancelable(true);
+                builder.setTitle("Cricket Academy");
+                builder.setMessage(buffer.toString());
+                builder.show();
             }
         });
 
