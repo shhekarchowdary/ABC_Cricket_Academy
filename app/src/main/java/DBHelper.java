6216eec33e133e4a61e3490438de5e0a99658c43
Manikantareddy1 <51731900+Manikantareddy1@users.noreply.github.com>
2021-02-01 23:07:02 +0530

    hello

---
+++ app/src/main/java/DBHelper.java
@@ -0,0 +1,88 @@
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(Context context) {
+        super(context, "CricketAcademy.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase DB) {
+        DB.execSQL("create Table CricketAcademy(playerid primarykey,name TEXT, dob TEXT )");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase DB, int oldVersion, int newVersion) {
+              DB.execSQL("drop Table if exists CricketAcademy");
+    }
+    public Boolean insertuserdata(String playerid, String firstname,String lastname,String dob,String height,String weight,String skill,String houseno,String street,String city,String zipcode,)
+    {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("playerid", playerid);
+        contentValues.put("firstname", firstname);
+        contentValues.put("lastname", name);
+        contentValues.put("dob", dob);
+        contentValues.put("firstname", name);
+        contentValues.put("firstname", name);
+        contentValues.put("firstname", name);
+        contentValues.put("firstname", name);
+        contentValues.put("firstname", name);
+        contentValues.put("firstname", name);
+        contentValues.put("dob", dob);
+        long result=DB.insert("CricketAcademy",null,contentValues);
+        if(result==-1) {
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public Boolean updateuserdata(String playerid, String name, String dob )
+    {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("playerid", playerid);
+        contentValues.put("name", name);
+        contentValues.put("dob", dob);
+        Cursor cursor = DB.rawQuery("Select*from CricketAcademy where id=?", new String[]  {name});
+        if(cursor.getCount()>0) {
+            long result = DB.update("CricketAcademy", contentValues, "id=?", new String[]{name});
+        if (result == -1) {
+                return false;
+        } else {
+                return true;
+        }
+        }else{
+            return false;
+        }
+    }
+    public Boolean deleteuserdata(String playerid, String name, String dob )
+    {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery("Select*from CricketAcademy where id=?", new String[]  {name});
+        if(cursor.getCount()>0) {
+            long result = DB.delete("CricketAcademy", "id=?", new String[]{name});
+            if (result == -1) {
+                return false;
+            } else {
+                return true;
+            }
+            }else {
+            return false;
+        }
+    }
+    public Cursor getdata()
+    {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery("Select*from CricketAcademy",null);
+        return cursor;
+    }
+}
+
--- app/src/main/java/com/arr/abccricketacademy/MainActivity.java
+++ app/src/main/java/com/arr/abccricketacademy/MainActivity.java
@@ -2,13 +2,56 @@ package com.arr.abccricketacademy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button Add;
+    Button ViewData;
+    Button Update;
+    Button Delete;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Add=findViewById(R.id.add);
+        ViewData=findViewById(R.id.viewdata);
+        Update=findViewById(R.id.update);
+        Delete=findViewById(R.id.delete);
+        Add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View v) {
+                Intent intent=new Intent(MainActivity.this,Add.class);
+                startActivity(intent);
+            }
+        });
+        ViewData.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(android.view.View v) {
+                        Intent intentview=new Intent(MainActivity.this,ViewData.class);
+                        startActivity(intentview);
+                    }
+
+        } );
+        Update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View v) {
+                Intent intentupdate=new Intent(MainActivity.this,Update.class);
+                startActivity(intentupdate);
+            }
+
+        } );
+        Delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View v) {
+                Intent intentdelete=new Intent(MainActivity.this,Delete.class);
+                startActivity(intentdelete);
+            }
+
+        } );
     }
 }
