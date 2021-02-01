44131c5f226fcd683c0bd1c033aca3f76a909ace
somasekharanaparthi <shhekarchowdary@gmail.com>
2021-02-01 22:28:04 +0530

    3 layouts

--- app/src/main/java/com/arr/abccricketacademy/MainActivity.java
+++ app/src/main/java/com/arr/abccricketacademy/MainActivity.java
@@ -10,6 +10,9 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button mAdd_Button;
+    Button mView_Button;
+    Button mUpdate_Button;
+    Button mDelete_Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +20,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAdd_Button = findViewById(R.id.add_button);
+        mView_Button = findViewById(R.id.view_button);
+        mUpdate_Button = findViewById(R.id.update_button);
+        mDelete_Button = findViewById(R.id.delete_button);
 
         mAdd_Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 Intent intent = new Intent(getApplicationContext(), add_player.class);
+                startActivity(intent);
+
+            }
+        });
+
+        mView_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), activity_view_player.class);
+                startActivity(intent);
+
+            }
+        });
+
+        mUpdate_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), activity_view_player.class);
+                startActivity(intent);
+
+            }
+        });
+
+        mDelete_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), activity_view_player.class);
                 startActivity(intent);
 
             }
